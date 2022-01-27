1173760bbff32ecb80a99a3a64a3328a242d2477
Suzette Senephansiri <93677348+Suzernator@users.noreply.github.com>
2022-01-27 15:23:18 -0600

    Create AllTests.java

---
+++ Week 2/IceCream/src/test/AllTests.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author SSene - Suzette Senephansiri
+ * CIS175 - Spring 2022
+ * Jan 26, 2022
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestLogic.class })
+public class AllTests {
+
+}
